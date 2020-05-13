61baf24b0b803811d99f27721d352ad53a1c591f
bicky633 <bicky633@gmail.com>
2020-05-13 13:54:40 +0530

    value annotation implementation

--- src/main/java/com/workshop/springiocdemo/SpringiocdemoApplication.java
+++ src/main/java/com/workshop/springiocdemo/SpringiocdemoApplication.java
@@ -1,13 +1,32 @@
 package com.workshop.springiocdemo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringiocdemoApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SpringiocdemoApplication.class, args);
+	@Bean
+	ValueBean myBean() {
+		return new ValueBean();
 	}
 
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(SpringiocdemoApplication.class, args);
+		ValueBean myBean = context.getBean(ValueBean.class);
+		myBean.startApplication();
+	}
+
+	private static class ValueBean {
+
+		@Value("${app.title}")
+		private String appTitle;
+
+		public void startApplication() {
+			System.out.printf("-- running application: %s --%n", appTitle);
+
+		}
+	}
 }
