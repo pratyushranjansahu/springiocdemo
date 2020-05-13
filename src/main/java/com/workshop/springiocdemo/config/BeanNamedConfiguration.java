7dcc8437d58f09ed48ce1d7082db48c535a0f760
bicky633 <bicky633@gmail.com>
2020-05-13 18:14:40 +0530

    description added to bew annotation

--- src/main/java/com/workshop/springiocdemo/config/BeanNamedConfiguration.java
+++ src/main/java/com/workshop/springiocdemo/config/BeanNamedConfiguration.java
@@ -3,6 +3,7 @@ package com.workshop.springiocdemo.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Description;
 import org.springframework.context.annotation.PropertySource;
 
 import com.workshop.springiocdemo.beans.BeanNameHelloMessage;
@@ -14,6 +15,7 @@ public class BeanNamedConfiguration {
 	private String message;
 	
 	@Bean(name={"myMessage", "motd"})
+	@Description("Provides a basic example of a bean")
     public BeanNameHelloMessage helloMessageProducer() {
 
 		BeanNameHelloMessage helloMessage = new BeanNameHelloMessage(message);
