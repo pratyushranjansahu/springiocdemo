5ec3fdce3e29678351fe04ee7a1bf95fafadd1f2
bicky633 <bicky633@gmail.com>
2020-05-13 15:25:03 +0530

    Response body implementation

--- src/main/java/com/workshop/springiocdemo/controller/SteroController.java
+++ src/main/java/com/workshop/springiocdemo/controller/SteroController.java
@@ -1,17 +1,27 @@
 package com.workshop.springiocdemo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.workshop.springiocdemo.service.SteroService;
 
+//@Controller
 @RestController
+@RequestMapping("/stero")
 public class SteroController {
 	@Autowired
 	private SteroService steroService;
 	
 	public SteroController(){
-		System.out.println("Acts As a controller");
+		System.out.println("Acts As a  Rest Controller");
+	}
+	@GetMapping
+	@ResponseBody
+	public String getString(){
+		return "From Rest Controller";
 	}
 
 }
