93c290adf76f77b8748ae15878a4c689fa81bfa8
bicky633 <bicky633@gmail.com>
2020-05-14 14:56:21 +0530

    Profile client modified with StandardEnvironment

--- src/main/java/com/workshop/springiocdemo/ClientProfile.java
+++ src/main/java/com/workshop/springiocdemo/ClientProfile.java
@@ -2,20 +2,24 @@ package com.workshop.springiocdemo;
 
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.StandardEnvironment;
 
 import com.workshop.springiocdemo.config.ProfileAppConfig;
 
 public class ClientProfile {
 
 	public static void main(String[] args) {
-		  AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-		  //Enable a "live" profile
-		  context.getEnvironment().setActiveProfiles("prod");
-		  
-		  context.register(ProfileAppConfig.class);
-		  context.refresh();
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+		// Enable a "prod" profile
+		// context.getEnvironment().setActiveProfiles("prod");
+		ConfigurableEnvironment environment = new StandardEnvironment();
+		environment.setActiveProfiles("dev");
 
-		  ((ConfigurableApplicationContext) context).close();
+		context.register(ProfileAppConfig.class);
+		context.refresh();
+
+		((ConfigurableApplicationContext) context).close();
 
 	}
 
