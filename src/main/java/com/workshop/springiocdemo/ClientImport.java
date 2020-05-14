4994d6f6abce10c5ba234d952e6d50440c3844f1
bicky633 <bicky633@gmail.com>
2020-05-14 15:50:12 +0530

    reading property value from property using Environment

--- src/main/java/com/workshop/springiocdemo/ClientImport.java
+++ src/main/java/com/workshop/springiocdemo/ClientImport.java
@@ -1,6 +1,9 @@
 package com.workshop.springiocdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
 
 import com.workshop.springiocdemo.beans.ImportABean;
 import com.workshop.springiocdemo.beans.ImportBBean;
@@ -9,11 +12,13 @@ import com.workshop.springiocdemo.config.ImportOneConfiguration;
 public class ClientImport {
 
 	public static void main(String[] args) {
+
 		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
 		ctx.register(ImportOneConfiguration.class);
 		ctx.refresh();
-		ImportABean a=(ImportABean)ctx.getBean("a");
-		ImportBBean b=(ImportBBean)ctx.getBean("b");
+
+		ImportABean a = (ImportABean) ctx.getBean("a");
+		ImportBBean b = (ImportBBean) ctx.getBean("b");
 		a.doWork();
 		b.doWork();
 		ctx.registerShutdownHook();
--- src/main/java/com/workshop/springiocdemo/SpringiocdemoApplication.java
+++ src/main/java/com/workshop/springiocdemo/SpringiocdemoApplication.java
@@ -1,23 +1,32 @@
 package com.workshop.springiocdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
-public class SpringiocdemoApplication {
+public class SpringiocdemoApplication implements CommandLineRunner{
+	@Autowired
+	private Environment environment;
 	@Bean
 	ValueBean myBean() {
 		return new ValueBean();
 	}
+	
 
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(SpringiocdemoApplication.class, args);
 		ValueBean myBean = context.getBean(ValueBean.class);
 		myBean.startApplication();
+	
 	}
+	
+
 
 	private static class ValueBean {
 
@@ -28,5 +37,11 @@ public class SpringiocdemoApplication {
 			System.out.printf("-- running application: %s --%n", appTitle);
 
 		}
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Printing Value using Environmental Value : "+environment.getProperty("app.title"));
+		
 	}
 }
