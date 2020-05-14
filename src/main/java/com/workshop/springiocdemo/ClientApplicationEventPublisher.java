a6a1b7dff1d49bc71f3fc2f4b2a5c19a01795bab
bicky633 <bicky633@gmail.com>
2020-05-14 15:30:20 +0530

    ApplicationEventPublisher implementation

---
+++ src/main/java/com/workshop/springiocdemo/ClientApplicationEventPublisher.java
@@ -0,0 +1,64 @@
+package com.workshop.springiocdemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
+
+public class ClientApplicationEventPublisher {
+
+	@Bean
+	AListenerBean listenerBean() {
+		return new AListenerBean();
+	}
+
+	@Bean
+	MyEvenPublisherBean publisherBean() {
+		return new MyEvenPublisherBean();
+	}
+
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
+				ClientApplicationEventPublisher.class);
+		MyEvenPublisherBean bean = context.getBean(MyEvenPublisherBean.class);
+		bean.sendMsg("A test message");
+
+	}
+
+	private static class MyEvenPublisherBean {
+		@Autowired
+		ApplicationEventPublisher publisher;
+
+		public void sendMsg(String msg) {
+			publisher.publishEvent(new MyEvent(msg));
+
+		}
+
+	}
+
+	private static class AListenerBean {
+
+		@EventListener
+		public void onMyEvent(MyEvent event) {
+			System.out.print("event received: " + event.getMsg());
+
+		}
+	}
+
+	/**
+	 * This time we are not extending any ApplicationEvent
+	 */
+	private static class MyEvent {
+		private final String msg;
+
+		public MyEvent(String msg) {
+			this.msg = msg;
+		}
+
+		public String getMsg() {
+			return msg;
+		}
+	}
+
+}
